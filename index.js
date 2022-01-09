91f47e0b67f254037598521d1a8e6b9854d34681
Ben Holland <bhollan5@kent.edu>
2022-01-09 10:25:06 -0500

    Fixing styling issues

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 //  Function to do single page app thing. 
 function goto(page) {
   //  Changing the page's URL without triggering HTTP call...
-  window.history.pushState({page: "/"}, "", page);
+  page == 'landing' ? 
+    window.history.pushState({page: "/"}, "", "/") : 
+    window.history.pushState({page: "/"}, "", page);
 
   //  Now we'll do the HTTP call here, to keep the SPA frame...
   const http = new XMLHttpRequest();
@@ -31,3 +33,27 @@ if ( window.location.pathname.split('/')[1]) {
 } else {
   goto('landing');
 }
+
+
+//  Code for demo 1;
+// class Demo1 extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { demo: false };
+//   }
+
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked this.';
+//     }
+
+//     return React.createElement(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
+
+// const domContainer = document.querySelector('#demo1');
+// ReactDOM.render(React.createElement(Demo1), domContainer);
